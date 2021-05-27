44442691392b3328f66809f818c06de0baf00383
Anirudh Gupta <firefinchdev@gmail.com>
2021-05-27 20:19:25 +0530

    async operations

--- GoogleTranslate/gtranslate.py
+++ GoogleTranslate/gtranslate.py
@@ -172,40 +172,63 @@
 
 # This subroutine calls Google translate and extracts the translation from
 # the html request
-def translate(to_translate, to_language="auto", language="auto"):
+
+async def translate(to_translate, to_language="auto", language="auto"):
+    excepted=False
+    while True:
+        try:
+            result = await translate_internal(to_translate, to_language, language)
+            if excepted:
+                print("FUCK: Success")
+            return result
+        except:
+            if excepted:
+                print("FUCK: Exception Again")
+            else:
+                print("FUCK: Exception")
+            excepted = True
+
+async def translate_internal(to_translate, to_language="auto", language="auto"):
     to_translate=serialize_text(to_translate, language)
     # send request
-    r = requests.get("https://translate.google.com/m?sl=%s&tl=%s&q=%s&op=translate"% (language, to_language, to_translate.replace(" ", "+")))
-    # set markers that enclose the charset identifier
-    beforecharset='charset='
-    aftercharset='" http-equiv'
-    # extract charset
-    parsed1=r.text[r.text.find(beforecharset)+len(beforecharset):]
-    parsed2=parsed1[:parsed1.find(aftercharset)]
-    # Display warning when encoding mismatch
-    if(parsed2!=r.encoding):
-        print('\x1b[1;31;40m' + 'Warning: Potential Charset conflict' )
-        print(" Encoding as extracted by SELF    : "+parsed2)
-        print(" Encoding as detected by REQUESTS : "+r.encoding+ '\x1b[0m')
+    session_timeout = aiohttp.ClientTimeout(total=5)
+    async with aiohttp.ClientSession(trust_env=True, timeout=session_timeout) as session:
+        translate_url = "https://translate.google.com/m?sl=%s&tl=%s&q=%s&op=translate"% (language, to_language, to_translate.replace(" ", "+"))
+        async with session.get(translate_url) as r:
+            # r = requests.get("https://translate.google.com/m?sl=%s&tl=%s&q=%s&op=translate"% (language, to_language, to_translate.replace(" ", "+")))
+            # set markers that enclose the charset identifier
+            beforecharset='charset='
+            aftercharset='" http-equiv'
+            # extract charset
+            text = await r.text()
+            text_encoding = r.get_encoding()
+            parsed1=text[text.find(beforecharset)+len(beforecharset):]
+            parsed2=parsed1[:parsed1.find(aftercharset)]
+            # Display warning when encoding mismatch
+            if(parsed2!=text_encoding):
+                pass
+                print('\x1b[1;31;40m' + 'Warning: Potential Charset conflict' )
+                print(" Encoding as extracted by SELF    : "+parsed2)
+                print(" Encoding as detected by REQUESTS : "+text_encoding+ '\x1b[0m')
 
-    # Work around an AGE OLD Python bug in case of windows-874 encoding
-    # https://bugs.python.org/issue854511
-    if(r.encoding=='windows-874' and os.name=='posix'):
-        print('\x1b[1;31;40m' + "Alert: Working around age old Python bug (https://bugs.python.org/issue854511)\nOn Linux, charset windows-874 must be labeled as charset cp874"+'\x1b[0m')
-        r.encoding='cp874'
+            # Work around an AGE OLD Python bug in case of windows-874 encoding
+            # https://bugs.python.org/issue854511
+            if(text_encoding=='windows-874' and os.name=='posix'):
+                print('\x1b[1;31;40m' + "Alert: Working around age old Python bug (https://bugs.python.org/issue854511)\nOn Linux, charset windows-874 must be labeled as charset cp874"+'\x1b[0m')
+                text_encoding='cp874'
 
-    # convert html tags
-    text=html.unescape(r.text)
-    # set markers that enclose the wanted translation
-    before_trans = 'class="result-container">'
-    after_trans='</div>'
-    # extract translation and return it
-    parsed1=r.text[r.text.find(before_trans)+len(before_trans):]
-    parsed2=parsed1[:parsed1.find(after_trans)]
-    # fix parameter strings
-    parsed3 = re.sub('% ([ds])', r' %\1', parsed2)
-    parsed4 = re.sub('% ([\d]) \$ ([ds])', r' %\1$\2', parsed3).strip()
-    return deserialize_text(html.unescape(parsed4).replace("'", r"\'"))
+            # convert html tags
+            # text=html.unescape(r.text)
+            # set markers that enclose the wanted translation
+            before_trans = 'class="result-container">'
+            after_trans='</div>'
+            # extract translation and return it
+            parsed1=text[text.find(before_trans)+len(before_trans):]
+            parsed2=parsed1[:parsed1.find(after_trans)]
+            # fix parameter strings
+            parsed3 = re.sub('% ([ds])', r' %\1', parsed2)
+            parsed4 = re.sub('% ([\d]) \$ ([ds])', r' %\1$\2', parsed3).strip()
+            return deserialize_text(html.unescape(parsed4).replace("'", r"\'"))
 
 
 
@@ -224,6 +247,7 @@ import shutil
 from pathlib import Path
 import asyncio
 import urllib.parse
+import aiohttp
 
 # ask user for paramters, apply defaults
 INFILE = sys.argv[1]
@@ -232,7 +256,7 @@ OUTPUTlangs = sys.argv[3:]
 
 if not OUTPUTlangs:
     OUTPUTlangs = ["af","sq","am","ar","hy","az","eu","be","bn","bs","bg","ca","ceb","ny","zh-CN","co","hr","cs","da","nl","en","eo","et","tl","fi","fr","fy","gl","ka","de","el","gu","ht","ha","haw","iw","hi","hmn","hu","is","ig","id","ga","it","ja","jw","kn","kk","km","rw","ko","ku","ky","lo","la","lv","lt","lb","mk","mg","ms","ml","mt","mi","mr","mn","my","ne","no","or","ps","fa","pl","pt","pa","ro","ru","sm","gd","sr","st","sn","sd","si","sk","sl","so","es","su","sw","sv","tg","ta","tt","te","th","tr","tk","uk","ur","ug","uz","vi","cy","xh","yi","yo","zu"]
-
+    OUTPUTlangs.remove(INPUTLANGUAGE)
 if not INFILE:
     INFILE = "strings.xml"
 if not INPUTLANGUAGE:
@@ -286,19 +310,19 @@ async def perform_translate(OUTPUTLANGUAGE):
     #	for each translatable string call the translation subroutine
     #   and replace the string by its translation,
     #   descend into each string array
-        time.sleep(1)
+    #     time.sleep(1)
         isTranslatable=root[i].get('translatable')
         if(root[i].tag=='string') & (isTranslatable!='false'):
             # trasnalte text and fix any possible issues traslotor creates: messing up HTML tags, adding spaces between string formatting elements
             totranslate=root[i].text
             if(totranslate!=None):
-                root[i].text=translate(totranslate,OUTPUTLANGUAGE,INPUTLANGUAGE)
+                root[i].text=await translate(totranslate,OUTPUTLANGUAGE,INPUTLANGUAGE)
 
             # if string was broken down due to HTML tags, reassemble it
             if len(root[i]) != 0:
                 for element in range(len(root[i])):
-                    root[i][element].text = " " + translate(root[i][element].text, OUTPUTLANGUAGE, INPUTLANGUAGE)
-                    root[i][element].tail = " " + translate(root[i][element].tail, OUTPUTLANGUAGE, INPUTLANGUAGE)
+                    root[i][element].text = " " + await translate(root[i][element].text, OUTPUTLANGUAGE, INPUTLANGUAGE)
+                    root[i][element].tail = " " + await translate(root[i][element].tail, OUTPUTLANGUAGE, INPUTLANGUAGE)
 
         if(root[i].tag=='string-array'):
             for j in range(len(root[i])):
@@ -309,14 +333,13 @@ async def perform_translate(OUTPUTLANGUAGE):
                     # trasnalte text and fix any possible issues traslotor creates: messing up HTML tags, adding spaces between string formatting elements
                     totranslate=root[i][j].text
                     if(totranslate!=None):
-                        root[i][j].text=translate(totranslate,OUTPUTLANGUAGE,INPUTLANGUAGE)
+                        root[i][j].text=await translate(totranslate,OUTPUTLANGUAGE,INPUTLANGUAGE)
 
                     # if string was broken down due to HTML tags, reassemble it
                     if len(root[i][j]) != 0:
                         for element in range(len(root[i][j])):
-                            root[i][j][element].text = " " + translate(root[i][j][element].text, OUTPUTLANGUAGE, INPUTLANGUAGE)
-                            root[i][j][element].tail = " " + translate(root[i][j][element].tail, OUTPUTLANGUAGE, INPUTLANGUAGE)
-
+                            root[i][j][element].text = " " + await translate(root[i][j][element].text, OUTPUTLANGUAGE, INPUTLANGUAGE)
+                            root[i][j][element].tail = " " + await translate(root[i][j][element].tail, OUTPUTLANGUAGE, INPUTLANGUAGE)
     # write new xml file
     tree.write(OUTFILE, encoding='utf-8')
 
@@ -328,5 +351,5 @@ async def start_translate():
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
-result = loop.run_until_complete(start_translate())
+loop.run_until_complete(start_translate())
 print("done")
